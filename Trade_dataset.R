ae38f1226c7cc6523baecb830c32d68850596422
Sandra Alemayehu <50651688+San-dra@users.noreply.github.com>
2020-02-14 00:36:50 +0100

    Initial Dataset preparation

---
+++ Trade_dataset.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(plyr)
+
+getwd()
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+
+
+#Load all trade dataset####
+#from https://www.usitc.gov/data/gravity/dataset.htm
+
+release1948_1977<-read.csv("C:/Users/sandr/Desktop/Social Network Analysis/Social-Network-Analysis-NAFTA/release_1.0_1948_1977.csv")
+release1978_1992<-read.csv("C:/Users/sandr/Desktop/Social Network Analysis/Social-Network-Analysis-NAFTA/release_1.0_1978_1992.csv")
+release1993_2004<-read.csv("C:/Users/sandr/Desktop/Social Network Analysis/Social-Network-Analysis-NAFTA/release_1.0_1993_2004.csv")
+release2005_2016<-read.csv("C:/Users/sandr/Desktop/Social Network Analysis/Social-Network-Analysis-NAFTA/release_1.0_2005_2016.csv")
+colnames(release1948_1977)==colnames(release1978_1992)
+colnames(release1978_1992)==colnames(release1993_2004)
+colnames(release1993_2004)==colnames(release2005_2016)
+colnames(release1948_1977)
+
+release1948_2016<-do.call("rbind", list(release1948_1977,release1978_1992,release1993_2004,release2005_2016))
+#Dataset for 22 years before and 22 after NAFTA came into effect in 1994####
+release1971_2016<-release1948_2016[release1948_2016$year>=1971, ]
+
+
+#subset data for Canada, USA  and Mexico.####
+#only trade relation of the three countries with the rest of the world
+CA_US_MX_1971_2016<-release1971_2016[release1971_2016$country_o %in% c("Canada", "United States","Mexico"), ]
+
+
+
+
+
+
