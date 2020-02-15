82290125ca7ab3250e64d1510b881af88a9ade03
Sandra Alemayehu <50651688+San-dra@users.noreply.github.com>
2020-02-15 16:49:36 +0100

    mapping network

--- Network1971-1993.R
+++ Network1971-1993.R
@@ -7,11 +7,12 @@ library(ggplot)
 #Creating Edges and nodes 1971-1993... directed graph    
 #(working with one year for similicity)
 release71_93_peryear<-split(release1971_1993,release1971_1993$year)
-
+lapply(release71_93_peryear, dim)
 
 library(tidyverse)
 edge_list <- tibble(from = release71_93_peryear$`1971`$country_o, to = release71_93_peryear$`1971`$country_d)
 node_list <- tibble(id = (release71_93_peryear$`1971`$country_o))
+#change year to see below figures
 
 g<-graph.data.frame(edge_list,directed = T)
 g
@@ -26,12 +27,21 @@ plot(simplify(g))
 E(g)[from("Canada")]
 E(g)[from("Mexico")]
 E(g)[from("United States")]
-##all have 200 edges
-
+##all have the same number of edges
+plot(make_ego_graph(g,3,"Canada")[[1]])
+CA_US_MX<-split(CA_US_MX_1971_2016,CA_US_MX_1971_2016$year)
+edge_list <- tibble(from = CA_US_MX$`1971`$country_o, to = CA_US_MX$`1971`$country_d)
+node_list <- tibble(id = (CA_US_MX$`1971`$country_o))
+g<-graph.data.frame(edge_list,directed = T)
+plot(g)
+make_ego_graph(g,3,"Canada")[[1]]
 
 #add node attribute and edge attribute ####
+release1971=release1971_1993[release1971_2016$year==1971, ]
 
+dim(release1972)
 
-
+edge_list <- tibble(from = release71_93_peryear$`1971`$country_o, to = release71_93_peryear$`1971`$country_d)
+node_list <- tibble(id = (release71_93_peryear$`1971`$country_o))
 
 
