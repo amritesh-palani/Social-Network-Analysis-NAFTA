4e69b7d6d93a604c1ea79b9d253fa2e023403a45
Bibake <bibake@student.ie.edu>
2020-02-28 12:01:53 +0100

    Reorganize

--- BACI.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-library(plyr)
-
-my_files <- list.files("C:/Users/sandr/Desktop/SNA/Social-Network-Analysis-NAFTA/BACI_HS92/BACI_HS92_1995.csv")
-BACI1995<-read.csv("C:/Users/sandr/Desktop/Social Network Analysis/Social-Network-Analysis-NAFTA/BACI_HS92/BACI_HS92_1995.csv",sep = ";")
-head(BACI1995)
