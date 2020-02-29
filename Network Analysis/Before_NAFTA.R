906d38c63c75b2f3d7c2f3978828a8b8512642f0
amritesh-palani <58050299+amritesh-palani@users.noreply.github.com>
2020-02-29 19:35:57 +0100

    viz update
    update with viz

--- Network Analysis/Before_NAFTA.R
+++ Network Analysis/Before_NAFTA.R
@@ -3,8 +3,6 @@ install.packages("sand")
 library(sand)
 install_sand_packages()
 
-#Reading Data
-
 packages <- c("igraph","visNetwork","dplyr","ggplot")
 install.packages(packages)
 install.packages("igraph")
@@ -12,10 +10,9 @@ install.packages("igraph")
 library(igraph)
 library(tidyverse)
 library(haven)
-
 require(igraph)
 
-#Visulaizaing the Global trade network. 
+#Reading Data
 
 getwd()
 setwd("~/Documents/MBD/Term 2/Social Network Analysis/Group thesis/Datasets/BACI_HS92")
@@ -24,50 +21,67 @@ colnames(BACI1995)
 head(BACI1995)
 summary(BACI1995)
 
-#list.vertex.attributes(BACI1995)
-#plot(BACI1995)
-
-library(tidyverse)
-edge_list <- tibble(from = BACI1995$Exporter, to = BACI1995$Importer)
-node_list <- tibble(id = (BACI1995$VoT))
-
+#Visulaizaing the Global trade network. 
+edge_list <- tibble(from = BACI1995$Importer, to = BACI1995$Exporter)
+node_list <- tibble(id = (BACI1995$Exporter))
 g<-graph.data.frame(edge_list, directed = T)
 g
-E(g)  #Edges needs to be weigthed by edge attribute and filtered
+E(g)
+V(g)
+plot(g)
+#Edges needs to be weigthed by edge attribute (Vo) and filtered
+#Plotting weighted graph based on value of trade
+E(g)$weight<-BACI1995$VoT
 V(g)
 plot(g)
 #Simplified
 plot(simplify(g))
 
-#only map where canada, mexico and  United States 
-E(g)[from("CAN")]
+#only map canada, mexico and  United States based on VoT 
 E(g)[from("USA")]
+E(g)[from("CAN")]
 E(g)[from("MEX")]
 
-##Trade from Canada to US in 1995 
+##Isolating Trade of CANADA in 1995 based on VoT 
+edge_list <- tibble(from =BACI1995$Exporter, to =BACI1995$Importer)
+node_list <- tibble(id = (BACI1995$Exporter))
+g<-graph.data.frame(edge_list,directed = T)
+E(g)[from("CAN")]
+E(g)$weight<-BACI1995$VoT
+plot(g)
+plot(make_ego_graph(g,1,"CAN")[[1]])
+
+##Isolate Trade of USA in 1995 based on VoT 
+edge_list <- tibble(from =BACI1995$Exporter, to =BACI1995$Importer)
+node_list <- tibble(id = (BACI1995$Exporter))
+g<-graph.data.frame(edge_list,directed = T)
+E(g)[from("USA")]
+E(g)$weight<-BACI1995$VoT
+g
+plot(g)
 plot(make_ego_graph(g,3,"USA")[[1]])
 
-CA_US<-split(BACI1995,BACI1995$Exporter["CAN"])
-edge_list <- tibble(from = BACI1995$Exporter, to = BACI1995$Importer)
-node_list <- tibble(id = (CA_US$CAN))
+##Isolate Trade of USA in 1995
+edge_list <- tibble(from =BACI1995$Exporter, to =BACI1995$Importer)
+node_list <- tibble(id = (BACI1995$Exporter))
 g<-graph.data.frame(edge_list,directed = T)
+E(g)[from("MEX")]
+E(g)$weight<-BACI1995$VoT
+g
 plot(g)
-plot(make_ego_graph(g,3,"CAN")[[1]])
+plot(make_ego_graph(g,3,"MEX")[[1]])
 
-edge_list <- tibble(from = BACI1995$Exporter, to = BACI1995$Importer)
-node_list <- tibble(id = (BACI1995$Importer))
-
-####Countries that have trade agreements
+####Visulaize Countries that have trade agreements
 str(BACI1995) #starting from full dataset
 which( colnames(BACI1995)=="agree_pta_goods")
 which( colnames(BACI1995)=="agree_fta")
-
 
 BACI1995$total_trade_agreement <-(BACI1995$agree_pta_goods+
                                             BACI1995$agree_pta_services+BACI1995$agree_cu+
                                             BACI1995$agree_eia+BACI1995$agree_fta
                                           +BACI1995$agree_psa)
 BACI1995$total_trade_agreement
+head(BACI1995)
 #the above sum of the total trade agreement they have with eachother
 #using this to create edges
 
@@ -78,18 +92,18 @@ BACI1995$any_trade_agreement
 head(BACI1995)
 
 #subsetting on countries that have any agreements with eachother
-BACI1995<-BACI1995[BACI1995$any_trade_agreement==TRUE,]
-BACI1995$any_trade_agreement
+BACI1995_TA<-BACI1995[BACI1995$any_trade_agreement==TRUE,]
+BACI1995_TA$any_trade_agreement
 
 #MAPPING directed/ undirected graph for those with agreements
-edge_list <- tibble(from = BACI1995$Exporter, to = BACI1995$Importer)
-node_list <- tibble(id = (BACI1995$Importer))
+edge_list <- tibble(from = BACI1995_TA$Exporter, to = BACI1995_TA$Importer)
+node_list <- tibble(id = (BACI1995_TA$Importer))
 agr_g<-graph.data.frame(edge_list,directed = T)
 agr_g
 E(agr_g)  #Needs to be weigthed by edge attribute and filtered
 V(agr_g)
 plot(agr_g)
-plot(simplify(agr_g))
+plot(simplify(agr_g))ol
 
 #Hypothesis 1: How are economic indicators (GDP, trade def,unemployement, etc) 
 #related to network metrics (closeness, centrality, strength) ?
@@ -98,15 +112,21 @@ plot(simplify(agr_g))
 #Degree dv of a vertex v, in a network graph G = (V,E),counts the number of edges in E incident upon v.  
 #The collection { fd }d≥0 is called the degree distribution of G, 
 #and is simply a rescaling of the set of degree frequencies, formed from the original degree sequence
-library("igraphdata")
 install.packages("network")
 library(network)
 library(sna)
 
+edge_list <- tibble(from = BACI1995$Importer, to = BACI1995$Exporter)
+node_list <- tibble(id = (BACI1995$Exporter))
+g<-graph.data.frame(edge_list, directed = T)
+g
+E(g)
+E(g)$weight<-BACI1995$VoT
+V(g)
 ecount(g)
 vcount(g)
 
-plot(hist(degree(g)))
+hist(degree(g))
 hist(degree(g),col="lightblue",
      xlab="Vertex Degree",ylab="Frequency",main="")   
 
@@ -137,6 +157,7 @@ plot(d.g, a.nn.deg.g, log="xy",col="goldenrod",
 #Centrality measures.
 
 A <- get.adjacency(g, sparse=FALSE)
+
 g <- network::as.network.matrix(A)
 sna::gplot.target(g, degree(g), main="Degree", 
                   circ.lab = FALSE, circ.col="skyblue",
@@ -177,7 +198,53 @@ diameter(g.gc)
 vertex.connectivity(g.gc)
 
 
+#Visualizing the global tarde network. 
+
+setwd("~/Documents/MBD/Term 2/Social Network Analysis/Group thesis/Datasets/BACI_HS92")
+BACI1995<-read.csv("BACI1995_agg.csv",sep = ",")
+colnames(BACI1995)
+head(BACI1995)
+
+#Visulaizaing the Global trade network. 
 
 
+#check<-read.csv("routes_edges.csv")
+#nodesvalue<-read.csv("airport_nodes.csv")
+#edges <- data.frame(from = check$ï..source, to = check$destination,value=check$distance)
+#nodes<- data.frame(id= nodesvalue$airport, value=nodesvalue$value)
+
+install.packages("BiocManager")
+BiocManager::install("Rgraphviz")
+library(Rgraphviz)
+install.packages("visNetwork")
+library(visNetwork)
 
 
+getwd()
+setwd("~/Documents/MBD/Term 2/Social Network Analysis/Group thesis/Datasets/BACI_HS92")
+BACI1995<-read.csv("BACI1995_agg.csv",sep = ",")
+edges<- data.frame(from =  BACI1995$Importer, to = BACI1995$Exporter, value=BACI1995$gdp_wdi_const_o)
+nodes<- data.frame(id = unique(BACI1995$Importer))
+
+graph_plot<-visNetwork(nodes, edges, background = "white" ,) %>%
+        visOptions(highlightNearest = TRUE, collapse = TRUE,autoResize = TRUE ,) %>%
+        visEdges(arrows = "to", dashes = TRUE,smooth = FALSE ,shadow=TRUE, 
+                 color = c(color=rgb(0,0,0, 0.3),highlight="orange",hover=rgb(1,0.60,0, 0.5))) %>%
+        visNodes( shape="dot",font = list(color="black",background= "white", size= 30) ,shadow = TRUE ,
+                  color= c(background=rgb(0.0,0.8,0.6, 0.9) ,border=rgb(0,0,0, 1),highlight="white" ),
+                  mass= 2 ) %>%
+        visEvents(type = "once",startStabilizing = "function() {
+                  this.moveTo({scale:1})}") %>% visIgraphLayout() %>%
+        visPhysics(stabilization = TRUE)
+
+graph_plot
+
+visNetwork(nodes, edges, background = "white" ,)
+
+nodes_temp <- BACI1995[,c('Importer', 'VoT')]
+
+head(nodes_temp)
+
+class(nodes_temp$VoT)
+
+value <- aggregate(nodes_temp, by = list(nodes_temp$Importer), sum)
