9bb27f02a57dfeafc3ee08df0ae61194f010f421
Sandra Alemayehu <50651688+San-dra@users.noreply.github.com>
2020-02-29 18:56:47 +0100

    corrected error na.rm=True

--- Data Preperation/Gravity_dataset_prep.R
+++ Data Preperation/Gravity_dataset_prep.R
@@ -38,7 +38,6 @@ unique(gravity2015_2017$year)
 
 #Creating Gravity Dataset for each Period Agregated.####
 
-view(gravity1995_1999)
 #KEY Attribues are going to be: Origin country and destination country profile, bilateral aggrements and 
 #some features describing relationship between countries such as distance, language 
 #Refer to dataset dictionary for more information below variables.
@@ -46,70 +45,70 @@ view(gravity1995_1999)
 #Agregating 1995 to 1999####
 detach(package:plyr)
 gravity1995_1999_agg<-gravity1995_1999%>%group_by(gravity1995_1999$iso3_o,gravity1995_1999$iso3_d)%>%
-  summarise(landlocked_o=max(landlocked_o),landlocked_d=max(landlocked_d),
-            island_o=max(island_o), island_d=max(island_d), 
-            gdp_wdi_const_d=mean(gdp_wdi_const_d),gdp_wdi_const_o=mean(gdp_wdi_const_d),
-            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d),
-            distance= mean(distance),pop_d=mean(pop_d), pop_o=mean(pop_o),
-            contiguity=max(contiguity), agree_pta_goods=max(agree_pta_goods),agree_pta_services=max(agree_pta_services),
-            agree_cu=max(agree_cu),agree_eia=max(agree_eia),agree_fta=max(agree_fta),agree_psa=max(agree_psa),
-            agree_pta=max(agree_pta),polity_o=max(polity_o),polity_d=max(polity_d), common_language=max(common_language))
+  summarise(landlocked_o=max(landlocked_o,na.rm=TRUE),landlocked_d=max(landlocked_d,na.rm=TRUE),
+            island_o=max(island_o,na.rm=TRUE), island_d=max(island_d,na.rm=TRUE), 
+            gdp_wdi_const_d=mean(gdp_wdi_const_d,na.rm=TRUE),gdp_wdi_const_o=mean(gdp_wdi_const_d,na.rm=TRUE),
+            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o,na.rm=TRUE),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d,na.rm=TRUE),
+            distance= mean(distance,na.rm=TRUE),pop_d=mean(pop_d,na.rm=TRUE), pop_o=mean(pop_o,na.rm=TRUE),
+            contiguity=max(contiguity,na.rm=TRUE), agree_pta_goods=max(agree_pta_goods,na.rm=TRUE),agree_pta_services=max(agree_pta_services,na.rm=TRUE),
+            agree_cu=max(agree_cu,na.rm=TRUE),agree_eia=max(agree_eia,na.rm=TRUE),agree_fta=max(agree_fta,na.rm=TRUE),agree_psa=max(agree_psa,na.rm=TRUE),
+            agree_pta=max(agree_pta,na.rm=TRUE),polity_o=max(polity_o,na.rm=TRUE),polity_d=max(polity_d,na.rm=TRUE), common_language=max(common_language,na.rm=TRUE))
 str(gravity1995_1999_agg)
 
 write.csv(gravity1995_1999_agg, file = "../Output/Gravity/gravity1995_1999_agg.csv") 
 
 #Agregating 2000 to 2004####
 gravity2000_2004_agg<-gravity2000_2004%>%group_by(gravity2000_2004$iso3_o,gravity2000_2004$iso3_d)%>%
-  summarise(landlocked_o=max(landlocked_o),landlocked_d=max(landlocked_d),
-            island_o=max(island_o), island_d=max(island_d), 
-            gdp_wdi_const_d=mean(gdp_wdi_const_d),gdp_wdi_const_o=mean(gdp_wdi_const_d),
-            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d),
-            distance= mean(distance),pop_d=mean(pop_d), pop_o=mean(pop_o),
-            contiguity=max(contiguity), agree_pta_goods=max(agree_pta_goods),agree_pta_services=max(agree_pta_services),
-            agree_cu=max(agree_cu),agree_eia=max(agree_eia),agree_fta=max(agree_fta),agree_psa=max(agree_psa),
-            agree_pta=max(agree_pta),polity_o=max(polity_o),polity_d=max(polity_d), common_language=max(common_language))
+  summarise(landlocked_o=max(landlocked_o,na.rm=TRUE),landlocked_d=max(landlocked_d,na.rm=TRUE),
+            island_o=max(island_o,na.rm=TRUE), island_d=max(island_d,na.rm=TRUE), 
+            gdp_wdi_const_d=mean(gdp_wdi_const_d,na.rm=TRUE),gdp_wdi_const_o=mean(gdp_wdi_const_d,na.rm=TRUE),
+            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o,na.rm=TRUE),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d,na.rm=TRUE),
+            distance= mean(distance,na.rm=TRUE),pop_d=mean(pop_d,na.rm=TRUE), pop_o=mean(pop_o,na.rm=TRUE),
+            contiguity=max(contiguity,na.rm=TRUE), agree_pta_goods=max(agree_pta_goods,na.rm=TRUE),agree_pta_services=max(agree_pta_services,na.rm=TRUE),
+            agree_cu=max(agree_cu,na.rm=TRUE),agree_eia=max(agree_eia,na.rm=TRUE),agree_fta=max(agree_fta,na.rm=TRUE),agree_psa=max(agree_psa,na.rm=TRUE),
+            agree_pta=max(agree_pta,na.rm=TRUE),polity_o=max(polity_o,na.rm=TRUE),polity_d=max(polity_d,na.rm=TRUE), common_language=max(common_language,na.rm=TRUE))
 str(gravity2000_2004_agg)
 
 write.csv(gravity2000_2004_agg, file = "../Output/Gravity/gravity2000_2004_agg.csv") 
 
 #Agregating 2005 to 2009####
 gravity2005_2009_agg<-gravity2005_2009%>%group_by(gravity2005_2009$iso3_o,gravity2005_2009$iso3_d)%>%
-  summarise(landlocked_o=max(landlocked_o),landlocked_d=max(landlocked_d),
-            island_o=max(island_o), island_d=max(island_d), 
-            gdp_wdi_const_d=mean(gdp_wdi_const_d),gdp_wdi_const_o=mean(gdp_wdi_const_d),
-            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d),
-            distance= mean(distance),pop_d=mean(pop_d), pop_o=mean(pop_o),
-            contiguity=max(contiguity), agree_pta_goods=max(agree_pta_goods),agree_pta_services=max(agree_pta_services),
-            agree_cu=max(agree_cu),agree_eia=max(agree_eia),agree_fta=max(agree_fta),agree_psa=max(agree_psa),
-            agree_pta=max(agree_pta),polity_o=max(polity_o),polity_d=max(polity_d), common_language=max(common_language))
+  summarise(landlocked_o=max(landlocked_o,na.rm=TRUE),landlocked_d=max(landlocked_d,na.rm=TRUE),
+            island_o=max(island_o,na.rm=TRUE), island_d=max(island_d,na.rm=TRUE), 
+            gdp_wdi_const_d=mean(gdp_wdi_const_d,na.rm=TRUE),gdp_wdi_const_o=mean(gdp_wdi_const_d,na.rm=TRUE),
+            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o,na.rm=TRUE),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d,na.rm=TRUE),
+            distance= mean(distance,na.rm=TRUE),pop_d=mean(pop_d,na.rm=TRUE), pop_o=mean(pop_o,na.rm=TRUE),
+            contiguity=max(contiguity,na.rm=TRUE), agree_pta_goods=max(agree_pta_goods,na.rm=TRUE),agree_pta_services=max(agree_pta_services,na.rm=TRUE),
+            agree_cu=max(agree_cu,na.rm=TRUE),agree_eia=max(agree_eia,na.rm=TRUE),agree_fta=max(agree_fta,na.rm=TRUE),agree_psa=max(agree_psa,na.rm=TRUE),
+            agree_pta=max(agree_pta,na.rm=TRUE),polity_o=max(polity_o,na.rm=TRUE),polity_d=max(polity_d,na.rm=TRUE), common_language=max(common_language,na.rm=TRUE))
 str(gravity2005_2009_agg)
 
 write.csv(gravity2005_2009_agg, file = "../Output/Gravity/gravity2005_2009_agg.csv") 
 
 #Agregating 2010 to 2014####
 gravity2010_2014_agg<-gravity2010_2014%>%group_by(gravity2010_2014$iso3_o,gravity2010_2014$iso3_d)%>%
-  summarise(landlocked_o=max(landlocked_o),landlocked_d=max(landlocked_d),
-            island_o=max(island_o), island_d=max(island_d), 
-            gdp_wdi_const_d=mean(gdp_wdi_const_d),gdp_wdi_const_o=mean(gdp_wdi_const_d),
-            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d),
-            distance= mean(distance),pop_d=mean(pop_d), pop_o=mean(pop_o),
-            contiguity=max(contiguity), agree_pta_goods=max(agree_pta_goods),agree_pta_services=max(agree_pta_services),
-            agree_cu=max(agree_cu),agree_eia=max(agree_eia),agree_fta=max(agree_fta),agree_psa=max(agree_psa),
-            agree_pta=max(agree_pta),polity_o=max(polity_o),polity_d=max(polity_d), common_language=max(common_language))
+  summarise(landlocked_o=max(landlocked_o,na.rm=TRUE),landlocked_d=max(landlocked_d,na.rm=TRUE),
+            island_o=max(island_o,na.rm=TRUE), island_d=max(island_d,na.rm=TRUE), 
+            gdp_wdi_const_d=mean(gdp_wdi_const_d,na.rm=TRUE),gdp_wdi_const_o=mean(gdp_wdi_const_d,na.rm=TRUE),
+            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o,na.rm=TRUE),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d,na.rm=TRUE),
+            distance= mean(distance,na.rm=TRUE),pop_d=mean(pop_d,na.rm=TRUE), pop_o=mean(pop_o,na.rm=TRUE),
+            contiguity=max(contiguity,na.rm=TRUE), agree_pta_goods=max(agree_pta_goods,na.rm=TRUE),agree_pta_services=max(agree_pta_services,na.rm=TRUE),
+            agree_cu=max(agree_cu,na.rm=TRUE),agree_eia=max(agree_eia,na.rm=TRUE),agree_fta=max(agree_fta,na.rm=TRUE),agree_psa=max(agree_psa,na.rm=TRUE),
+            agree_pta=max(agree_pta,na.rm=TRUE),polity_o=max(polity_o,na.rm=TRUE),polity_d=max(polity_d,na.rm=TRUE), common_language=max(common_language,na.rm=TRUE))
 str(gravity2010_2014_agg)
 
 write.csv(gravity2010_2014_agg, file = "../Output/Gravity/gravity2010_2014_agg.csv") 
 
 #Agregating 2010 to 2014####
 gravity2015_2017_agg<-gravity2015_2017%>%group_by(gravity2015_2017$iso3_o,gravity2015_2017$iso3_d)%>%
-  summarise(landlocked_o=max(landlocked_o),landlocked_d=max(landlocked_d),
-            island_o=max(island_o), island_d=max(island_d), 
-            gdp_wdi_const_d=mean(gdp_wdi_const_d),gdp_wdi_const_o=mean(gdp_wdi_const_d),
-            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d),
-            distance= mean(distance),pop_d=mean(pop_d), pop_o=mean(pop_o),
-            contiguity=max(contiguity), agree_pta_goods=max(agree_pta_goods),agree_pta_services=max(agree_pta_services),
-            agree_cu=max(agree_cu),agree_eia=max(agree_eia),agree_fta=max(agree_fta),agree_psa=max(agree_psa),
-            agree_pta=max(agree_pta),polity_o=max(polity_o),polity_d=max(polity_d), common_language=max(common_language))
+  summarise(landlocked_o=max(landlocked_o,na.rm=TRUE),landlocked_d=max(landlocked_d,na.rm=TRUE),
+            island_o=max(island_o,na.rm=TRUE), island_d=max(island_d,na.rm=TRUE), 
+            gdp_wdi_const_d=mean(gdp_wdi_const_d,na.rm=TRUE),gdp_wdi_const_o=mean(gdp_wdi_const_d,na.rm=TRUE),
+            gdp_wdi_cap_const_o=mean(gdp_wdi_cap_const_o,na.rm=TRUE),gdp_wdi_cap_const_d=mean(gdp_wdi_cap_const_d,na.rm=TRUE),
+            distance= mean(distance,na.rm=TRUE),pop_d=mean(pop_d,na.rm=TRUE), pop_o=mean(pop_o,na.rm=TRUE),
+            contiguity=max(contiguity,na.rm=TRUE), agree_pta_goods=max(agree_pta_goods,na.rm=TRUE),agree_pta_services=max(agree_pta_services,na.rm=TRUE),
+            agree_cu=max(agree_cu,na.rm=TRUE),agree_eia=max(agree_eia,na.rm=TRUE),agree_fta=max(agree_fta,na.rm=TRUE),agree_psa=max(agree_psa,na.rm=TRUE),
+            agree_pta=max(agree_pta,na.rm=TRUE),polity_o=max(polity_o,na.rm=TRUE),polity_d=max(polity_d,na.rm=TRUE), common_language=max(common_language,na.rm=TRUE))
 str(gravity2015_2017_agg)
 
 write.csv(gravity2015_2017_agg, file = "../Output/Gravity/gravity2015_2017_agg.csv") 
